01f535dd7309a1d27dd5426dcf48f9966c1416dc
Dbibi <draven@dbibi.ca>
2025-12-15 00:57:38 -0500

    Initial Commit

---
+++ backend/src/main/java/com/yorku/betterticketmaster/BetterticketmasterApplication.java
@@ -0,0 +1,13 @@
+package com.yorku.betterticketmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterticketmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterticketmasterApplication.class, args);
+	}
+
+}
